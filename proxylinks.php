05e70fa45c55d7fd4f062f34be8d0b7a4c3546b3
waqar3 <waqar3@gmail.com>
2017-05-23 10:29:03 +0100

    added more descriptive comments

--- proxylinks.php
+++ proxylinks.php
@@ -14,30 +14,35 @@ class UrlExtract
 	
 	function __construct()
 	{
-		# code...
+		# sets value to split whole page on every <a> tag
+		#sets regular expression to scan URLs
 		$this->divideon = "<a";
 		$this->url_pattern = '/((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z0-9\&\.\/\?\:@\-_=#])*/';
 	}
 	function getWebpage(){
+		#gets html of a webpage
 		$this->webpage = file_get_html($this->url);
 	}
 	function divideHtml(){
+		#divides whole html page using a given divider
 		return explode($this->divideon, $this->webpage);
 
 	}
 	function getAllUrls(){
+		#matches pattern and extracts URL in a webpage
 		preg_match_all($this->url_pattern, $this->webpage, $matches);
 		$this->allurls = $matches[0];
 		
 	}
 	function getHostnames(){
+		#parses URLs finds hostnames from URLs
 		foreach ($this->allurls as $u) {
 		$hostnames[] = parse_url($u, PHP_URL_HOST);
 	}
 		return array_unique($hostnames);
 
 	}	function getSitePaths(){
-
+		#finds webpages internal links
 
 		$sitepaths = preg_grep('/^(\/)(.*)(\.html|\/)$/', $this->allurls);
 
@@ -53,7 +58,7 @@ $internallinks[] = 'http://homestead.app/anonymous/proxyindex1.html';
 
 $externallinks = array();
 foreach ($internallinks as $l) {
-	# code...
+	# creating a instance of UrlExtract class, and providing a URL to gather links from
 
 	
 	$proxyindex = new UrlExtract;
@@ -68,7 +73,7 @@ foreach ($internallinks as $l) {
 }
 
 foreach ($externallinks as $link) {
-	# code...
+	# dumping links array for test
 	echo $link;
 	 var_dump(get_headers("http://".$link));
 	
